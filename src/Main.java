b6e184a4b08d8b9a6c43fb2416501917ea24093e
PacoBarni <paco12cv@hotmail.com>
2023-02-24 12:19:18 +0100

    oh padre mío

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
+		System.out.println(3);
+		System.out.printf("5 + 6 + 7 = %d", 5 + 6 + 7).println();
+		System.out.printf("%d + %d + %d = %d", 5, 6, 7, 5 + 6 + 7).println();
 	}
 
 }
